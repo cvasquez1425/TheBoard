f62c4c2a40e989e3d785900172c0a1648c421a58
u748 <carlos.vasquez@gmlaw.com>
2018-12-07 11:02:42 -0500

    Commit Vash View Engine code 12/07/2018

--- TheBoard/server.js
+++ TheBoard/server.js
@@ -3,11 +3,16 @@ var express = require("express");  // require express to start up our server
 var app = express();                // the express dependency reference can be executed like a function, and it'll return to us an object that represent our individual app, a singleton
 
 //Setup the view engine. We don't need a require here because we're giving it the name of it.
-app.set("view engine", "jade");
+//app.set("view engine", "jade");
+
+//Vash View Engine
+app.set("view engine", "vash");
 
 app.get("/", function (req, res) {
     //res.send("<html><body><h1>Express</h1></body></html>");  //the response now has helpers. send is a helper function to avoid write and end, get you there a little bit quicker.
-    res.render("jade/index", { title: "Express + Jade" });
+    //res.render("jade/index", { title: "Express + Jade" });
+    //res.render("ejs/index", { title: "Express + Ejs" });
+    res.render("index", { title: "Express + Vash" });
 });
 
 app.get("/api/users", function (req, res) {
